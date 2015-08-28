58533b70e0347bb10b6c5b13a121cc9a03b04df3
Jorge Luis De Alba <jorlu934@gmail.com>
2015-08-27 22:52:22 -0500

    Nivel para exploracion de controles y script de cameraShake

---
+++ SlothishRunner/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour 
+{
+	private static CameraShake _instance;
+	public static CameraShake instance
+	{
+        get
+        {
+            if(_instance == null)
+                _instance = GameObject.FindObjectOfType<CameraShake>();
+            return _instance;
+        }
+    }
+
+	private float _amplitude;
+	private Vector3 initialPosition;
+	private bool isShaking;
+	
+	void Start()
+	{
+		initialPosition = transform.localPosition;
+	}
+	
+	void Update () 
+	{
+		if(isShaking)
+		{
+			transform.localPosition = initialPosition + Random.insideUnitSphere * _amplitude;
+		}
+	}
+
+	public void Shake(float amplitude, float duration)
+	{
+		_amplitude = amplitude;
+		isShaking = true;
+
+		CancelInvoke();
+		Invoke("StopShaking", duration);
+	}
+
+	public void StopShaking()
+	{
+		isShaking = false;
+	}
+}
