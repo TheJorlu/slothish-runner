949713b5061f6aaecc089d8acdfe8bcd34fd24d8
Ander <amr.martin10@gmail.com>
2015-10-18 17:41:21 -0500

    Scripts de sistema de armas

---
+++ SlothishRunner/Assets/Scripts/GenericGun.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericGun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawnPos;
+	private bool canShoot;
+//	private bool isReloading;
+	private float reloadTimer;
+//	private float fireRateTimer;
+	public float fireRateCooldown;
+	public float reloadTime;
+	public int totalAmmo;
+	private int ammoClip;
+
+	// Use this for initialization
+	void Start () {
+		canShoot = true;
+		//isReloading = false;
+		reloadTimer = reloadTime;
+		//fireRateTimer = 0;
+		ammoClip = totalAmmo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && canShoot) {
+			Shoot ();
+		}
+	}
+
+	/*void GetFireRateUpdate()
+	{
+		if (fireRateTimer < fireRateCooldown) {
+			fireRateTimer += Time.fixedDeltaTime;
+			canShoot = false;
+			if(fireRateTimer >= fireRateCooldown)
+			{
+				canShoot = true;
+			}
+		}
+	}*/
+
+	/*bool GetReloadUpdate()
+	{
+		if (reloadTimer < reloadTime) {
+			reloadTimer += Time.fixedDeltaTime;
+			canShoot = false;
+		}
+	}*/
+
+	void Reload()
+	{
+		if (ammoClip == totalAmmo) {
+			return;
+		}
+		canShoot = false;
+		StartCoroutine ("GunReload");
+	}
+
+	IEnumerator GunReload()
+	{
+		/*while (reloadTimer < reloadTime) {
+			reloadTimer += Time.fixedDeltaTime;
+			yield return new WaitForSeconds(Time.fixedDeltaTime);
+		}*/
+		yield return new WaitForSeconds (reloadTime);
+		ammoClip = totalAmmo;
+		canShoot = true;
+
+	}
+
+	IEnumerator TimeBetweenShots()
+	{
+		yield return new WaitForSeconds (fireRateCooldown);
+		canShoot = true;
+	}
+
+	void Shoot()
+	{
+		if (ammoClip == 0) {
+			Reload ();
+		} else {
+			ammoClip--;
+			Instantiate(bulletPrefab, bulletSpawnPos.position, Quaternion.identity);
+			bulletPrefab.GetComponent<RocketMissile>().SetDirection(this.transform.forward);
+			canShoot = false;
+			StartCoroutine("TimeBetweenShots");
+		}
+	}
+}
---
+++ SlothishRunner/Assets/Scripts/RocketMissile.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class RocketMissile : MonoBehaviour {
+	public float radius;
+	public float power;
+	public GameObject RocketPrefab;
+	public Vector3 direction;
+	public float speed;
+
+	/*RocketMissile(Vector3 direction)
+	{
+		this.transform = direction;
+	}*/
+
+	public void SetDirection(Vector3 _direction)
+	{
+		direction = _direction;
+	}
+	
+	void Start() {
+		StartCoroutine ("SelfDestroy");
+	}
+
+	void Update() {
+		this.transform.Translate (direction * speed);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			Rigidbody rb = hit.GetComponent<Rigidbody>();
+			
+			if (rb != null){
+				rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+				Debug.Log ("RigidBodyFound, Adding Force");
+			}
+			
+		}
+
+		Destroy (this.gameObject);
+	}
+
+	IEnumerator SelfDestroy()
+	{
+		yield return new WaitForSeconds (5f);
+		Destroy (this.gameObject);
+	}
+}
