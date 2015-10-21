e75c70d60bb99e87280ef1c3cddbdbb6199c2cff
AnderMrt <amr.martin10@gmail.com>
2015-10-21 13:05:58 -0500

    RaceScene

---
+++ SlothishRunner/Assets/Scripts/RaceTimer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceTimer : MonoBehaviour {
+
+	public TextMesh TimeDisplay;
+	float TotalTime;
+	bool running;
+
+	// Use this for initialization
+	void Start () {
+		running = false;
+		TotalTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (running) {
+			TotalTime += Time.deltaTime;
+		}
+		TimeDisplay.text = TotalTime.ToString("#.##");
+	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.gameObject.name == "Start") {
+			running = true;
+			Debug.Log ("Start");
+		}
+
+		if (hit.gameObject.name == "Finish") {
+			running = false;
+			Debug.Log("Final");
+		}
+	}
+}
